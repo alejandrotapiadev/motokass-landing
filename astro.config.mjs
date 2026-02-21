79de2aff4a53ef648be8111221cea241fa061d71
Alejandro Tapia <tapiacaballeroalejandro684@gmail.com>
2026-02-21 17:35:58 +0100

    analytics vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless'; // si usas Vercel
 
+import react from '@astrojs/react';
+
 export default defineConfig({
-  output: "server", 
+  output: "server",
   adapter: vercel({}),
+  integrations: [react()],
 });
